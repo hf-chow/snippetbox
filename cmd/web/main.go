8854a75f83a142de487d927e46831dd2ac491941
hf-chow <honfungchow@gmail.com>
2024-07-28 00:16:33 +0800

    add flags and base template

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+    "flag"
     "log"
     "net/http"
+    "os"
 )
 
+type config struct {
+    addr        string
+    staticDir   string
+}
+
 func main() {
+    var cfg config
+
+    flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+    flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+
+    flag.Parse()
+
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    errorLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
     mux := http.NewServeMux()
 
-    mux.HandleFunc("/", home)
+    fileServer := http.FileServer(http.Dir(cfg.staticDir))
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+    mux.HandleFunc("/", home)
     mux.HandleFunc("/snippet/view", snippetView)
     mux.HandleFunc("/snippet/create", snippetCreate)
 
 
-    log.Println("Starting server on :4000")
-    err := http.ListenAndServe(":4000", mux)
+    log.Printf("Starting server on %s", cfg.addr)
+    err := http.ListenAndServe(cfg.addr, mux)
     log.Fatal(err)
 }
